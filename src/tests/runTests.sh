fe6af0c704fb216980806b1cfa5da0a896a946db
Lorenzo Bertin Salvador <lorenzobs@usp.br>
2022-10-13 21:16:08 -0300

    fix: organize file path names correctly
    Signed-off-by: Lorenzo Bertin Salvador <lorenzobs@usp.br>

--- src/tests/runTests.sh
+++ src/tests/runTests.sh
@@ -7,34 +7,45 @@ hc_quantity=8
 for number in $(seq 1 $hc_quantity)
 do
 
+	hc_file="hc$number.lp"
+
+	input_sat_file="hc$number\_input_sat.lp"
+	input_unsat_file="hc$number\_input_unsat.lp"
+	
+	sat_exp_file="hc$number\_sat_exp.txt"
+
+	output_unsat_file="/tmp/hc$number\_out_unsat.txt"
+	output_sat_file="/tmp/clingo_hc$number_out_sat.txt"
+
+
 	# Test if all files needed exist
 
-	if ! [ -f hc$number.lp -a -f hc$number\_input_unsat.lp -a -f hc$number\_input_sat.lp -a -f hc$number\_sat_exp.txt  ]; then
+	if ! [ -f $hc_file -a -f $input_unsat_file -a -f  $input_sat_file -a -f  $sat_exp_file ]; then
 	    echo "Hard constraint $number does not have all files needed... Aborting its tests"
 	    continue
 	fi
 
 	# UNSAT tests
 
-	clingo basic_constraints.lp hc$number.lp hc$number\_input_unsat.lp > /tmp/hc$number\_out_unsat.txt 0 2>/dev/null
+	clingo basic_constraints.lp $hc_file $input_unsat_file 0 > $output_unsat_file  2>/dev/null
 
-	if ! grep -q "UNSATISFIABLE" /tmp/hc$number\_out_unsat.txt; then
+	if ! grep -q "UNSATISFIABLE" $output_unsat_file; then
 		echo "Error UNSAT constraint $number"
 	fi
 
-	rm /tmp/hc$number\_out_unsat.txt
+	rm $output_unsat_file
 
 	# SAT tests
 
-	clingo basic_constraints.lp hc$number.lp hc$number\_input_sat.lp 0 > /tmp/clingo_hc$number_out_sat.txt 2>/dev/null
+	clingo basic_constraints.lp $hc_file $input_sat_file 0 > $output_sat_file  2>/dev/null
 
-	answer=$(python sat-answers-comparator.py hc$number\_sat_exp.txt < /tmp/clingo_hc$number_out_sat.txt)
+	answer=$(python sat-answers-comparator.py $sat_exp_file < $output_sat_file)
 
 	if [ $answer != "True" ]; then
 		echo "Error SAT constraint $number"
 	fi
 
-	rm /tmp/clingo_hc$number_out_sat.txt
+	rm $output_sat_file
 
 	echo "Tests completed for hc$number"
 
