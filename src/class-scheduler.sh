e9251874740713f174bd6bde86b2aaa838cb3de8
Daniel Pessoa Cardeal <danielpessoa.cardeal@gmail.com>
2022-11-04 13:49:18 -0300

    feat: add num_models cli args to the POC script

--- src/class-scheduler.sh
+++ src/class-scheduler.sh
@@ -11,7 +11,45 @@ BASIC_CONSTRAINTS="$CONSTRAINTS_DIR/basic_constraints.lp"
 HARD_CONSTRAINTS=$(find "$CONSTRAINTS_DIR" -name "hc*[0-9].lp")
 INPUT="$BASE_DIR/input.lp"
 
-clingo 1 \
+# Print usage
+usage() {
+	cat <<EOF
+USAGE:
+    class-scheduler [OPTIONS]         Run the class scheduler POC
+
+OPTIONS:
+    -h | --help: print this help message
+    -n | --num-models <number>: number of models to be generated, defaults to 1
+EOF
+}
+
+# Parse CLI args
+num_models=1
+
+while [[ $# -gt 0 ]]; do
+	case "$1" in
+	-h | --help)
+		usage
+		exit 0
+		;;
+	-n | --num-models)
+		num_models="$2"
+		shift
+		shift
+		;;
+	-* | --*)
+		echo "WARN: unknown option $1"
+		shift
+		;;
+	*)
+		echo default
+		exit 1
+		;;
+	esac
+done
+
+# Runs the clingo interpreter
+clingo $num_models \
 	$BASIC_CONSTRAINTS \
 	$HARD_CONSTRAINTS \
 	$INPUT | python3 $OUTPUT_PARSER
