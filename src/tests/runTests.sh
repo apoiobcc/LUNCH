c566999013af5beee3464e91a8e4b81879fd7ff4
Daniel Pessoa Cardeal <danielpessoa.cardeal@gmail.com>
2022-11-04 15:48:41 -0300

    fix: remove comments of old version of the script

--- src/tests/runTests.sh
+++ src/tests/runTests.sh
@@ -71,46 +71,4 @@ for hc_file in $HARD_CONSTRAINTS; do
 			echo "  $test_filename [OK]" ||
 			echo "  $test_filename [FAIL]"
 	done
-
-	# input_sat_file="$ABS_PATH/hc${number}_input_sat.lp"
-	# input_unsat_file="$ABS_PATH/hc${number}_input_unsat.lp"
-	#
-	# sat_exp_file="$ABS_PATH/hc${number}_sat_exp.txt"
-	#
-	# output_unsat_file="/tmp/hc${number}_out_unsat.txt"
-	# output_sat_file="/tmp/clingo_hc${number}_out_sat.txt"
-	#
-	# # Test if all files needed exist
-	#
-	# if ! [ -f $hc_file -a -f $input_unsat_file -a -f $input_sat_file -a -f $sat_exp_file ]; then
-	# 	echo "Hard constraint $number does not have all files needed... Aborting its tests"
-	# 	continue
-	# fi
-	#
-	# # UNSAT tests
-	#
-	# clingo $basic_hc_file $hc_file $input_unsat_file 0 >$output_unsat_file 2>/dev/null
-	#
-	# if ! grep -q "UNSATISFIABLE" $output_unsat_file; then
-	# 	echo "Error UNSAT constraint $number"
-	# fi
-	#
-	# rm $output_unsat_file
-	#
-	# # SAT tests
-	#
-	# clingo $basic_hc_file $hc_file $input_sat_file 0 >$output_sat_file 2>/dev/null
-	#
-	# answer=$(python $sat_answers_comparator_path $sat_exp_file <$output_sat_file)
-	#
-	# if [ $answer != "True" ]; then
-	# 	echo "Error SAT constraint $number"
-	# fi
-	#
-	# rm $output_sat_file
-	#
-	# echo "Tests completed for hc$number"
-	#
 done
-
-# echo "Done"
