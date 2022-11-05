945717d03de00607ae9b1a739f69625ecf07ef54
Lorenzo Bertin Salvador <lorenzobs@usp.br>
2022-11-05 10:58:20 -0300

    feat: change the script to allow soft constraints execution
    Signed-off-by: Lorenzo Bertin Salvador <lorenzobs@usp.br>

--- src/class-scheduler.sh
+++ src/class-scheduler.sh
@@ -5,13 +5,19 @@
 
 BASE_DIR=$(dirname $0)
 CONSTRAINTS_DIR="${BASE_DIR}/constraints"
-OUTPUT_PARSER=$(find "$BASE_DIR" -name "clingo_output_parser.py")
+OUTPUT_PARSER=$(find "$BASE_DIR" -name "clingo-output-parser.py")
 
 BASIC_CONSTRAINTS="$CONSTRAINTS_DIR/basic_constraints.lp"
+MINIMIZE_SC="$CONSTRAINTS_DIR/minimize_sc.lp"
+PYTHON_OPS="$CONSTRAINTS_DIR/python_utils.lp"
 HARD_CONSTRAINTS=$(find "$CONSTRAINTS_DIR" -name "hc*[0-9].lp")
+SOFT_CONSTRAINTS=$(find "$CONSTRAINTS_DIR" -name "sc*[0-9].lp")
 INPUT="$BASE_DIR/input.lp"
 
-clingo 1 \
+clingo 0 \
+	$MINIMIZE_SC \
+	$PYTHON_OPS \
 	$BASIC_CONSTRAINTS \
 	$HARD_CONSTRAINTS \
+	$SOFT_CONSTRAINTS \
 	$INPUT | python3 $OUTPUT_PARSER
