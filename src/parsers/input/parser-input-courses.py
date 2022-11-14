2584cf7f594cb7b19147d429ebdfbfff4d77adeb
Daniel Pessoa Cardeal <danielpessoa.cardeal@gmail.com>
2022-11-14 19:58:45 -0300

    fix: format Python source code with Isort and Black

--- src/parsers/input/parser-input-courses.py
+++ src/parsers/input/parser-input-courses.py
@@ -1,9 +1,9 @@
-'''
+"""
 Clingo Input Courses Parser (Fixed)
 ----------------------
 This program can parse a given csv files containing fixed information about the courses.
 Input: csv table containing the course information of a certain predicate (fill up the dictionary).
-Output: clingo input files in the clingo_input_files directory. 
+Output: clingo input files in the clingo_input_files directory.
 The names of each file will be the predicate name.
 If the file already exists, it will append the new information.
 
@@ -18,58 +18,70 @@ Create a directory named clingo_input_files
 
 $ python3 parser-input-courses.py
 
-'''
+"""
 
-import sys
 import csv
+import sys
+
 
 def parser(predicate, n_args, file_name):
-    '''
-        Given the predicate name, number of arguments and file containing the information (csv),
-        this function returns a long string containing all the clausules assembled
-    '''
+    """
+    Given the predicate name, number of arguments and file containing the information (csv),
+    this function returns a long string containing all the clausules assembled
+    """
     with open(file_name) as csv_file:
         headers = next(csv_file)
-        csv_reader = csv.reader(csv_file, delimiter=',')
+        csv_reader = csv.reader(csv_file, delimiter=",")
         clingo_input = ""
         for row in csv_reader:
             atom = predicate + "("
-            for i in range(n_args-1):
+            for i in range(n_args - 1):
                 atom += row[i].lower() + ","
-            atom += row[n_args-1].lower() + ")."
+            atom += row[n_args - 1].lower() + ")."
             clingo_input += atom + "\n"
     return clingo_input
-	        
-def main():	
+
+
+def main():
     arqs = [
-            {   'predicate' : "curriculum",
-                'n_args' : 2,
-                'file_name' : "csv_input/Disciplinas - Trilhas.csv",
-            },
-            {   'predicate' : "obligatory",
-                'n_args' : 2,
-                'file_name' : "csv_input/Disciplinas - Obrigatorias.csv",
-            },
-            {   'predicate' : "num_classes",
-                'n_args' : 2,
-                'file_name' : "csv_input/Disciplinas - Graduacao.csv",
-            },
-            {   'predicate' : "num_classes",
-                'n_args' : 2,
-                'file_name' : "csv_input/Disciplinas - Pos-Graduacao.csv",
-            },
-            {   'predicate' : "postgrad",
-                'n_args' : 1,
-                'file_name' : "csv_input/Disciplinas - Pos-Graduacao.csv",
-            },
-            {   'predicate' : "curriculum",
-                'n_args' : 3,
-                'file_name' : "csv_input/Disciplinas - (Pos-Grad) Trilhas.csv",
-            }]
+        {
+            "predicate": "curriculum",
+            "n_args": 2,
+            "file_name": "csv_input/Disciplinas - Trilhas.csv",
+        },
+        {
+            "predicate": "obligatory",
+            "n_args": 2,
+            "file_name": "csv_input/Disciplinas - Obrigatorias.csv",
+        },
+        {
+            "predicate": "num_classes",
+            "n_args": 2,
+            "file_name": "csv_input/Disciplinas - Graduacao.csv",
+        },
+        {
+            "predicate": "num_classes",
+            "n_args": 2,
+            "file_name": "csv_input/Disciplinas - Pos-Graduacao.csv",
+        },
+        {
+            "predicate": "postgrad",
+            "n_args": 1,
+            "file_name": "csv_input/Disciplinas - Pos-Graduacao.csv",
+        },
+        {
+            "predicate": "curriculum",
+            "n_args": 3,
+            "file_name": "csv_input/Disciplinas - (Pos-Grad) Trilhas.csv",
+        },
+    ]
 
     for arq in arqs:
-        clingo_input = parser(arq['predicate'], arq['n_args'], arq['file_name'])
-        with open(f"clingo_input_files/{arq['predicate']}.txt", 'a') as clingo_input_file:
+        clingo_input = parser(arq["predicate"], arq["n_args"], arq["file_name"])
+        with open(
+            f"clingo_input_files/{arq['predicate']}.txt", "a"
+        ) as clingo_input_file:
             clingo_input_file.write(clingo_input)
+
 
 main()
--- src/parsers/output/test_output-parser.py
+++ src/parsers/output/test_output-parser.py
@@ -1,44 +1,67 @@
-import pytest
 import sys
+
+import pytest
 from clingo_output_support import *
 
+
 class TestOutputParser:
-    def setup_class(self):    
+    def setup_class(self):
         with open("clingo-output-sat.txt") as f:
             raw = f.read()
             answers_list = parse_input(raw)
             self.sat_answers = []
             for a in answers_list:
                 sched = make_sched(a)
-                head,body = make_table(sched)
-                self.sat_answers.append((head,body))
+                head, body = make_table(sched)
+                self.sat_answers.append((head, body))
 
     def test_header(self):
         head = self.sat_answers[0][0]
-        correct_head = ['Horário', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta']
+        correct_head = ["Horário", "Segunda", "Terça", "Quarta", "Quinta", "Sexta"]
         assert head == correct_head
 
     def test_sat1(self):
         body = self.sat_answers[0][1]
-        correct_body = [['08:00-09:40', '', '', '', 'macBBB-45\n', 'macAAA-45\nmacBBB-45\n'], ['10:00-11:40', '', '', '', '', ''], ['14:00-15:40', '', '', '', 'macAAA-45\nmacBBB-45\n', ''], ['16:00-17:40', '', '', '', '', '']]
+        correct_body = [
+            ["08:00-09:40", "", "", "", "macBBB-45\n", "macAAA-45\nmacBBB-45\n"],
+            ["10:00-11:40", "", "", "", "", ""],
+            ["14:00-15:40", "", "", "", "macAAA-45\nmacBBB-45\n", ""],
+            ["16:00-17:40", "", "", "", "", ""],
+        ]
         for b, cb in tuple(zip(body, correct_body)):
             assert b == cb
-    
+
     def test_sat2(self):
         body = self.sat_answers[1][1]
-        correct_body = [['08:00-09:40', '', '', '', 'macAAA-45\nmacBBB-45\n', 'macAAA-45\nmacBBB-45\n'], ['10:00-11:40', '', '', '', '', ''], ['14:00-15:40', '', '', '', 'macBBB-45\n', ''], ['16:00-17:40', '', '', '', '', '']]
+        correct_body = [
+            [
+                "08:00-09:40",
+                "",
+                "",
+                "",
+                "macAAA-45\nmacBBB-45\n",
+                "macAAA-45\nmacBBB-45\n",
+            ],
+            ["10:00-11:40", "", "", "", "", ""],
+            ["14:00-15:40", "", "", "", "macBBB-45\n", ""],
+            ["16:00-17:40", "", "", "", "", ""],
+        ]
         for b, cb in tuple(zip(body, correct_body)):
             assert b == cb
-    
+
     def test_sat3(self):
         body = self.sat_answers[2][1]
-        correct_body = [['08:00-09:40', '', '', '', 'macAAA-45\nmacBBB-45\n', 'macBBB-45\n'], ['10:00-11:40', '', '', '', '', ''], ['14:00-15:40', '', '', '', 'macAAA-45\nmacBBB-45\n', ''], ['16:00-17:40', '', '', '', '', '']]
+        correct_body = [
+            ["08:00-09:40", "", "", "", "macAAA-45\nmacBBB-45\n", "macBBB-45\n"],
+            ["10:00-11:40", "", "", "", "", ""],
+            ["14:00-15:40", "", "", "", "macAAA-45\nmacBBB-45\n", ""],
+            ["16:00-17:40", "", "", "", "", ""],
+        ]
         for b, cb in tuple(zip(body, correct_body)):
             assert b == cb
-    
+
     def test_unsat(self):
         with open("clingo-output-unsat.txt") as f:
             raw = f.read()
             answers_list = parse_input(raw)
             assert answers_list == False
-
