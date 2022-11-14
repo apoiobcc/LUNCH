1b7d51e8b8c41e36b1bdee2486585dffd38e2d89
jyns1703 <jyns1703@usp.br>
2022-11-14 17:40:47 -0300

    doc: Write comments and documentation
    Signed-off-by: jyns1703 <jyns1703@usp.br>

--- src/parsers/input/parser-input-teacher-schedule.py
+++ src/parsers/input/parser-input-teacher-schedule.py
@@ -1,76 +1,136 @@
+'''
+Clingo Input Teachers Schedule Parser (Semestral)
+----------------------
+This program can parse a given csv files containing the restrictions and preferences for each teacher.
+Input: path to csv file containing the information.
+Output: files containing asp clausules for the availability and preferable times of the teachers  
+The names of each file will be the predicate name and semester/year.
+(Ex: input file = 2s22.csv, output file = available2s22.txt and preferable2s22.txt)
+If the file already exists, it will append the new information.
+
+Running: 
+
+Create a directory named clingo_input_files 
+
+$ python3 parser-input-teacher-schedule.py
+
+'''
+
 import sys
 import csv
 
+'''
+This list represents all periods codes that a class can be given
+'''
 ALLPERIODS = [11, 12, 21, 22]
 
+'''
+Function that transforms a day in its code
+'''
 def transformDay(d):
-        if (d == 'Segunda'): return 100
-        if (d == 'Terça'): return 200
-        if (d == 'Quarta'): return 300
-        if (d == 'Quinta'): return 400
-        if (d == 'Sexta'): return 500
-        return 0
+    if (d == 'Segunda'): return 100
+    if (d == 'Terça'): return 200
+    if (d == 'Quarta'): return 300
+    if (d == 'Quinta'): return 400
+    if (d == 'Sexta'): return 500
+    return 0
 
+'''
+Function that transforms a period in its code
+'''
 def transformPeriod(p):
-        if (p.startswith("08")): return 11
-        if (p.startswith("10")): return 12
-        if (p.startswith("14")): return 21
-        if (p.startswith("16")): return 22
-        return 0
+    if (p.startswith("08")): return 11
+    if (p.startswith("10")): return 12
+    if (p.startswith("14")): return 21
+    if (p.startswith("16")): return 22
+    return 0
 
-def getProfessorName(email):
-        return email.split('@')[0].lower().replace('.', '')
+def getTeacherName(email):
+    return email.split('@')[0].lower().replace('.', '')
 
 def getDayHeader(header):
-        return header.split('[')[1][:-1]
+    return header.split('[')[1][:-1]
 
-def getProfessorSched(file_name):
-        with open(file_name) as csv_file:
-                infos = list()
-                csv_reader = csv.reader(csv_file, delimiter=',')
-                for row in csv_reader:
-                        headers = row
-                        break
-                for row in csv_reader:
-                        atom = dict()
-                        atom['professor'] = getProfessorName(row[1])
-                        atom['preference'] = dict()
-                        atom['restriction'] = dict()
-                        for i in range(2,7):
-                                day = transformDay(getDayHeader(headers[i]))
-                                periods = list()
-                                for p in row[i].split(';'):
-                                        if (transformPeriod(p) == 0): continue
-                                        periods.append(transformPeriod(p))
-                                atom['preference'][day] = periods
-                        for i in range(7,12):
-                                day = transformDay(getDayHeader(headers[i]))
-                                periods = list()
-                                for p in row[i].split(';'):
-                                        if (transformPeriod(p) == 0): continue
-                                        periods.append(transformPeriod(p))
-                                atom['restriction'][day] = periods
-                        atom['available'] = availablePeriod(atom['restriction'])
-                        infos.append(atom)
-        return infos
+'''
+This function receives a csv file that contains the columns:
+0-time_stamp | 1-teacher_email | 2-6 preferable_periods [day of the week] | 7-11 restricted_periods [day of the week]
 
-def availablePeriod(restriction):
-        available = dict()
-        for key in restriction.keys():
+And returns a list of dictionaries
+The dictionaries keys are: teacher, preference, restriction, available.
+The values for preference, restriction, available are another list of dictionaries 
+that contains the time codes for each category
+'''
+def getTeacherSched(file_name):
+    with open(file_name) as csv_file:
+        infos = list()
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        # get header
+        for row in csv_reader:
+            headers = row
+            break
+        for row in csv_reader:
+            atom = dict()
+            atom['teacher'] = getTeacherName(row[1])
+            atom['preference'] = dict()
+            atom['restriction'] = dict()
+            # get teachers preferences
+            for i in range(2,7):
+                day = transformDay(getDayHeader(headers[i]))
                 periods = list()
-                for p in ALLPERIODS: periods.append(p)
-                for p in restriction[key]: periods.remove(p)
-                available[key] = periods
-        return available
-        
-def printDayPredicate(predicate, professor, periods):
-        for day in periods.keys():
-                for hour in periods[day]:
-                        p = day + hour
-                        print(f'{predicate}({professor},{p}).')
+                for p in row[i].split(';'):
+                    if (transformPeriod(p) == 0): continue
+                    periods.append(transformPeriod(p))
+                atom['preference'][day] = periods
+            # get teachers restrictions
+            for i in range(7,12):
+                day = transformDay(getDayHeader(headers[i]))
+                periods = list()
+                for p in row[i].split(';'):
+                    if (transformPeriod(p) == 0): continue
+                    periods.append(transformPeriod(p))
+                atom['restriction'][day] = periods
+            # get teachers availability
+            atom['available'] = availablePeriod(atom['restriction'])
+            infos.append(atom)
+    return infos
 
-file_name = sys.argv[1]
-info = getProfessorSched(file_name)
-for i in info:
-        printDayPredicate("available", i['professor'], i['available'])
-        # printDayPredicate("preference", i['professor'], i['preference'])
+'''
+Given the restriction times dictionary
+Returns a dictonary with the times not restricted
+'''
+def availablePeriod(restriction):
+    available = dict()
+    for key in restriction.keys():
+        periods = list()
+        for p in ALLPERIODS: periods.append(p)   
+        for p in restriction[key]: periods.remove(p)
+        available[key] = periods
+    return available
+
+'''
+Assembles asp clausules given the predicate, first argument(teacher) and 
+a list of the second arguments
+'''
+def assembleDayPredicate(predicate, teacher, periods):
+    assembled = ""
+    for day in periods.keys():
+        for hour in periods[day]:
+            # get time code with day and period
+            p = day + hour
+            assembled = assembled + (f'{predicate}({teacher},{p}).') + '\n'
+    return assembled
+
+def main():
+    file_name = sys.argv[1]
+    info = getTeacherSched(file_name)
+    available = ""
+    preferable = ""
+    for i in info:
+        available = available + assembleDayPredicate("available", i['teacher'], i['available'])
+        preferable = preferable + assembleDayPredicate("preference", i['teacher'], i['preference'])
+    with open(f"clingo_input_files/available{file_name[-8:-4]}.txt", 'a') as clingo_input_file:
+        clingo_input_file.write(available[:-1])
+    with open(f"clingo_input_files/preferable{file_name[-8:-4]}.txt", 'a') as clingo_input_file:
+        clingo_input_file.write(preferable[:-1])
+
+main()
--- src/parsers/input/parser-input-workload.py
+++ src/parsers/input/parser-input-workload.py
@@ -1,3 +1,21 @@
+'''
+Clingo Input Workload Parser (Semestral)
+----------------------
+This program can parse a given csv files containing the years workload (teacher-course relation).
+Input: path to csv file containing the information.
+Output: file containing asp clausules for the course predicate  
+The names of each file will be the predicate name and semester/year.
+(Ex: input file = 2023.csv, output file = course1s22.txt and course2s22.txt)
+If the file already exists, it will append the new information.
+
+Running: 
+
+Create a directory named clingo_input_files 
+
+$ python3 parser-input-workload.py
+
+'''
+
 import csv
 import sys
 import re
@@ -7,20 +25,27 @@ group_id = {"BCC" : 0} #stores the id for each group
 group_count = 0        #stores the number of groups (0 represents only bcc)
 
 def getGroupId(course_name):
+    # the group name is written between parenthesis
     start = course_name.find('(')
     if (start == -1): return 0
     end = course_name.find(')')
     group = course_name[start+1:end]
+    # indexing groups
     if (group not in group_id.keys()):
         global group_count
         group_count += 1
         group_id[group] = group_count
     return group_id[group]
 
+'''
+Receives the workload csv table 
+Returns a list of dictionaries that have the keys: teacher, course, semester
+'''
 def getWorkload(file_name):
     with open(file_name) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         info = list()
+        # get header
         for row in csv_reader:
             headers = row
             break
@@ -32,16 +57,29 @@ def getWorkload(file_name):
                 if course[0].isdigit(): atom['course'] = 'mac'+ course
                 else: atom['course'] = course
                 atom['group'] = getGroupId(row[1])
-                atom['professor'] = row[4].lower().replace('.', '')
+                atom['teacher'] = row[4].lower().replace('.', '')
                 atom['semester'] = int(row[2][0])
                 info.append(atom)
         return info
 
-def printWorkload(info, semester):
+'''
+Given the list of dictionary and semester,
+Assemble the asp input for the course predicate of the given semester
+'''
+def assembleWorkload(info, semester):
+    workload = ""
     for atom in info:
         if (atom['semester'] != semester): continue
-        print(f"course({atom['course']},{atom['group']},{atom['professor']}).")
+        workload = workload + f"course({atom['course']},{atom['group']},{atom['teacher']})." + '\n'
+    return workload[:-1]
 
+def main():
+    info = getWorkload(file_name)
+    s1 = assembleWorkload(info, 1)
+    s2 = assembleWorkload(info, 2)
+    with open(f"clingo_input_files/course1s{file_name[-6:-4]}.txt", 'a') as clingo_input_file:
+        clingo_input_file.write(s1)
+    with open(f"clingo_input_files/course2s{file_name[-6:-4]}.txt", 'a') as clingo_input_file:
+        clingo_input_file.write(s2)
 
-info = getWorkload(file_name)
-printWorkload(info, 1)
+main()
