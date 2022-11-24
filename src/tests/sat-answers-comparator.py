90fb808cf874b2cd4e04aedd38687a6d932170a1
Lorenzo Bertin Salvador <lorenzobs@usp.br>
2022-11-24 07:05:09 -0300

    fix: refactor the code to add the operation that tests if exp answer is contained in clingo answer
    Signed-off-by: Lorenzo Bertin Salvador <lorenzobs@usp.br>

--- src/tests/sat-answers-comparator.py
+++ src/tests/sat-answers-comparator.py
@@ -1,35 +1,33 @@
-"""
+'''
 Sat Answers Comparator
 ----------------------
-This program receives two files, one (from stdin) containing a clingo output and the other (from the argument) the expected output.
-The program compares both and decides if they are equal, meaning the model is correct.
+This program receives two files and one operation (equal or contains). One file is the clingo output and the other the expected output.
+If the operation is "equal", the program compares both and decides if they are completely equal.
+If the operation is "contains", the program compares both and decides if the expected answer is contained in the clingo answer.
 Input: clingo output and the expected output
-Output: Print true if both are equal and false if not
+Output: True or false
 
 Running Example
-$ python3 sat-answers-comparator.py hc1_expected_input_sat.txt < clingo_output_hc1.txt
-
+$ python sat-answers-comparator.py --expected hc1_expected_input_sat.txt --clingo clingo_output_hc1.txt --operation equal
 To run the tests
-$ python3 sat-answers-comparator.py tests
+$ python3 sat-answers-comparator.py --test
 
-"""
+'''
 
 import sys
 import unittest
-
+import argparse
 
 def parse_clingo_input(raw):
-    """
+    '''
     Separate each answer (from clingo) and returns a list with all classes scheduled
     If no answer is found, returns False
-    """
-
+    '''
+    
     parsed = raw.split("Answer: ")
     parsed.pop(0)
-
     # no answers
-    if not parsed:
-        return False
+    if(not parsed): return False
 
     answers_list = list()
     for p in parsed:
@@ -37,168 +35,215 @@ def parse_clingo_input(raw):
         answers_list.append(answer[1].split(" "))
     return answers_list
 
+def equal_answers(clingo,expected):
+	'''
+		Return true if answers are equal
+	'''
+	clingo_set = create_set(clingo)
+	expected_set = create_set(expected)
+	return clingo_set == expected_set
 
-def compare_answers(clingo, expected):
-    """
-    Compare two set of answers. If the set is equal returns true
-    If they are not equal, returns False
-    """
-    clingo_set = set()
-    expected_set = set()
+def contains(clingo,expected):
+	'''
+		Return true if the clingo answer contains the expected answer
+	'''
+	clingo_set = create_set(clingo)
+	if len(expected) == 0 and len(clingo) != 0: return False
+	for i in expected:
+		if set(i) not in clingo_set:
+			return False
+	return True
 
-    if len(clingo) != len(expected) or len(clingo) == 0:
-        return False
-
-    for i in range(0, len(clingo)):
-        clingo_set.add(frozenset(set(clingo[i])))
-        expected_set.add(frozenset(set(expected[i])))
-
-    return clingo_set == expected_set
-
+def create_set(answer):
+	'''
+		Create the set representation of an answer
+	'''
+	answer_set = set()
+	for i in range(0,len(answer)):
+		answer_set.add(frozenset(set(answer[i])))
+	return answer_set
 
 def parse_expected_input(raw):
-    """
+    '''
     Separate each answer (from expected output) and return a list with all classes scheduled
     If no answer is found, return False
-    """
+    '''
     parsed = raw.split("\n\n")
-    if not parsed:
-        return False
+    if (not parsed): return False
 
     answers_list = list()
     for p in parsed:
-        answer = p.replace("\n", "").split(" ")
-        if answer == [""]:
-            continue
-        answers_list.append(answer)
+    	answer = p.replace("\n","").rstrip().split(" ")
+    	if answer == [""]:
+    		continue
+    	answers_list.append(answer)
     return answers_list
 
+class TestClassEqualOp(unittest.TestCase):
+	'''
+		Test class to test the equal operation
+	'''	
+	## Tests inputs
+	out0 = [["class(A,11:00)","class(B,12:00)"]]
+	exp0 = [["class(A,11:00)","class(B,12:00)"]]
 
-def main(args):
-    FILE = args[1]
+	out1 = [["class(A,11:00)","class(B,12:00)"]]
+	exp1 = [["class(B,12:00)","class(A,11:00)"]] 
 
-    raw = sys.stdin.read()
-    clingo_answers_list = parse_clingo_input(raw)
+	out2 = [["class(A,11:00)"],["class(B,12:00)"],["class(C,11:00)"]]
+	exp2 = [["class(C,11:00)"],["class(B,12:00)"],["class(A,11:00)"]]
 
-    if not clingo_answers_list:
-        print("UNSAT")
-        return
+	out3 = [["class(A,11:00)","class(B,12:00)","class(C,13:00)"],["class(D,11:00)","class(E,12:00)"],["class(F,11:00)","class(G,13:00)"]]
+	exp3 = [["class(G,13:00)","class(F,11:00)"],["class(B,12:00)","class(A,11:00)","class(C,13:00)"],["class(E,12:00)","class(D,11:00)"]] 
 
-    try:
-        file = open(FILE, "r")
-    except:
-        print('Error reading file "', FILE, '"', sep="")
-        quit()
+	exp4 = [["class(G,13:00)","class(F,11:00)"],["class(B,12:00)","class(ZZZZZZZ,11:00)","class(C,13:00)"],["class(E,12:00)","class(D,11:00)"]]
 
-    expected_answers_list = parse_expected_input(file.read())
+	exp5 = [["class(G,13:00)","class(F,11:00)"],["class(B,12:00)","class(A,11:00)","class(C,13:00)"],["class(E,12:00)","class(D,11:00)"],["class(A,11:00)"]]
 
-    print(compare_answers(clingo_answers_list, expected_answers_list))
+	exp6 = [["class(G,13:00)","class(F,11:00)"],["class(B,12:00)","class(A,11:00)","class(C,13:00)"],["class(E,12:00)"]]
 
-    return
+	exp7 = [["class(G,13:00)","class(F,11:00)"],["class(B,12:00)","class(A,11:00)","class(C,13:00)"]]
 
+	
+	def test_true_trivial_answers(self):
+		self.assertTrue(equal_answers(self.out0,self.exp0),"Error in the trivial test")
+	def test_true_different_predicates_order(self):
+		self.assertTrue(equal_answers(self.out1,self.exp1),"Erro when changing predicates order")
+	def test_true_different_answers_order(self):
+		self.assertTrue(equal_answers(self.out2,self.exp2),"Error when changing answer order")
+	def test_true_different_answers_predicates_order(self):
+		self.assertTrue(equal_answers(self.out3,self.exp3),"Error when changing answers and predicates order")
+	def test_false_different_predicate(self):
+		self.assertFalse(equal_answers(self.out3,self.exp4),"Error when the predicate is different")
+	def test_false_extra_answer(self):
+		self.assertFalse(equal_answers(self.out3,self.exp5),"Error when the size of the answers sets doesn't match")
+	def test_false_missing_predicate(self):
+		self.assertFalse(equal_answers(self.out3,self.exp6),"Error when the size of the predicates set doesn't match")
+	def test_false_missing_answer(self):
+		self.assertFalse(equal_answers(self.out3,self.exp7),"Error when there is a missing answer")
 
-class TestClass(unittest.TestCase):
+class TestClassContainsOp(unittest.TestCase):
+	'''
+		Test class to test the contains operation
+	'''	
+	## Tests inputs
+	out0 = [["class(A,11:00)","class(B,12:00)"]]
+	exp0 = [["class(A,11:00)","class(B,12:00)"]]
 
-    ## Tests inputs
-    out0 = [["class(A,11:00)", "class(B,12:00)"]]
-    exp0 = [["class(A,11:00)", "class(B,12:00)"]]
+	out1 = [["class(A,11:00)","class(B,12:00)"]]
+	exp1 = [["class(B,12:00)","class(A,11:00)"]] 
 
-    out1 = [["class(A,11:00)", "class(B,12:00)"]]
-    exp1 = [["class(B,12:00)", "class(A,11:00)"]]
+	out2 = [["class(A,11:00)"],["class(B,12:00)"],["class(C,11:00)"]]
+	exp2 = [["class(C,11:00)"]]
 
-    out2 = [["class(A,11:00)"], ["class(B,12:00)"], ["class(C,11:00)"]]
-    exp2 = [["class(C,11:00)"], ["class(B,12:00)"], ["class(A,11:00)"]]
+	out3 = [["class(A,11:00)","class(B,12:00)","class(C,13:00)"],["class(D,11:00)","class(E,12:00)"],["class(F,11:00)","class(G,13:00)"]]
+	exp3 = [["class(G,13:00)","class(F,11:00)"],["class(B,12:00)","class(A,11:00)","class(C,13:00)"]] 
 
-    out3 = [
-        ["class(A,11:00)", "class(B,12:00)", "class(C,13:00)"],
-        ["class(D,11:00)", "class(E,12:00)"],
-        ["class(F,11:00)", "class(G,13:00)"],
-    ]
-    exp3 = [
-        ["class(G,13:00)", "class(F,11:00)"],
-        ["class(B,12:00)", "class(A,11:00)", "class(C,13:00)"],
-        ["class(E,12:00)", "class(D,11:00)"],
-    ]
+	exp4 = [["class(G,13:00)","class(F,11:00)"],["class(B,12:00)","class(ZZZZZZZ,11:00)","class(C,13:00)"],["class(E,12:00)","class(D,11:00)"]]
 
-    exp4 = [
-        ["class(G,13:00)", "class(F,11:00)"],
-        ["class(B,12:00)", "class(ZZZZZZZ,11:00)", "class(C,13:00)"],
-        ["class(E,12:00)", "class(D,11:00)"],
-    ]
+	exp5 = []
 
-    exp5 = [
-        ["class(G,13:00)", "class(F,11:00)"],
-        ["class(B,12:00)", "class(A,11:00)", "class(C,13:00)"],
-        ["class(E,12:00)", "class(D,11:00)"],
-        ["class(A,11:00)"],
-    ]
+	exp6 = [["class(G,13:00)","class(F,11:00)"],["class(Z,12:00)"]]
 
-    exp6 = [
-        ["class(G,13:00)", "class(F,11:00)"],
-        ["class(B,12:00)", "class(A,11:00)", "class(C,13:00)"],
-        ["class(E,12:00)"],
-    ]
+	exp7 = [["class(G,13:00)","class(Z,11:00)"],["class(B,12:00)","class(Z,11:00)","class(C,13:00)"]]
 
-    exp7 = [
-        ["class(G,13:00)", "class(F,11:00)"],
-        ["class(B,12:00)", "class(A,11:00)", "class(C,13:00)"],
-    ]
+	
+	def test_true_trivial_answers(self):
+		self.assertTrue(contains(self.out0,self.exp0),"Error in the trivial test (contains)")
+	def test_true_different_predicates_order(self):
+		self.assertTrue(contains(self.out1,self.exp1),"Erro when changing predicates order (contains)")
+	def test_true_one_equal_answer(self):
+		self.assertTrue(contains(self.out2,self.exp2),"Error when one answer is contained in the output")
+	def test_true_two_equal_answers(self):
+		self.assertTrue(contains(self.out3,self.exp3),"Error when two answers are contained in the output")
+	def test_false_different_predicate(self):
+		self.assertFalse(contains(self.out3,self.exp4),"Error when the predicate is different (contains)")
+	def test_false_empty_set(self):
+		self.assertFalse(contains(self.out3,self.exp5),"Error when the expected set is empty")
+	def test_false_one_answer_wrong(self):
+		self.assertFalse(contains(self.out3,self.exp6),"Error when one is not contained")
+	def test_false_all_wrong(self):
+		self.assertFalse(contains(self.out3,self.exp7),"Error when none is contained")
 
-    def test_true_trivial_answers(self):
-        self.assertTrue(
-            compare_answers(self.out0, self.exp0), "Error in the trivial test"
-        )
+def print_usage():
+	print("Usage: python sat-answers-comparator.py --expected {EXPECTED ANSWERS FILE} --clingo {CLINGO ANSWERS FILE} --operation {equal,contains} --test {to run the tests}")
 
-    def test_true_different_predicates_order(self):
-        self.assertTrue(
-            compare_answers(self.out1, self.exp1), "Erro when changing predicates order"
-        )
+class MyParser():
+    '''
+    	Arguments parser
+    '''
+    def __init__(self):
+        if len(sys.argv) == 1:
+            self.error()
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--expected',help='expected answer file')
+        parser.add_argument('--clingo',help='clingo answer file')
+        parser.add_argument('--operation',help='operations possible: equals,contains')
+        parser.add_argument('--test',action='store_true',default=False)
+        self.args = ''
+        try:
+            self.args = parser.parse_args()
+        except:
+            self.error()
 
-    def test_true_different_answers_order(self):
-        self.assertTrue(
-            compare_answers(self.out2, self.exp2), "Error when changing answer order"
-        )
+    def args(self):
+        return self.args
+    def error(self):
+        print_usage()
+        sys.exit(2)
 
-    def test_true_different_answers_predicates_order(self):
-        self.assertTrue(
-            compare_answers(self.out3, self.exp3),
-            "Error when changing answers and predicates order",
-        )
+def unittestTrigger():
+	unittest.main()
+	quit()
 
-    def test_false_different_predicate(self):
-        self.assertFalse(
-            compare_answers(self.out3, self.exp4),
-            "Error when the predicate is different",
-        )
+def main():
+	p = MyParser()
+	args = p.args
+	CLINGO_FILE = args.clingo
+	EXPECTED_FILE = args.expected
+	OPERATION = args.operation
+	TEST = args.test
 
-    def test_false_extra_answer(self):
-        self.assertFalse(
-            compare_answers(self.out3, self.exp5),
-            "Error when the size of the answers sets doesn't match",
-        )
+	if TEST == True:
+		sys.argv = [sys.argv[0]]
+		unittest.main(__name__,exit=False)
+		quit()
 
-    def test_false_missing_predicate(self):
-        self.assertFalse(
-            compare_answers(self.out3, self.exp6),
-            "Error when the size of the predicates set doesn't match",
-        )
+	if CLINGO_FILE == None or EXPECTED_FILE == None or OPERATION == None:
+		print_usage()
+		quit()
 
-    def test_false_missing_answer(self):
-        self.assertFalse(
-            compare_answers(self.out3, self.exp7),
-            "Error when there is a missing answer",
-        )
+	try:
+		raw = open(CLINGO_FILE,"r").read()
+	except:
+		print("Error reading file \"",raw,"\"",sep='')
+		quit()
+
+	clingo_answers_list = parse_clingo_input(raw)
+
+	if (not clingo_answers_list): 	
+		print("UNSAT")
+		return
+
+	try:
+		file = open(EXPECTED_FILE, "r")
+	except:
+		print("Error reading file \"",file,"\"",sep='')
+		quit()
+
+	expected_answers_list = parse_expected_input(file.read())
+
+	if OPERATION == "equal":
+		print(equal_answers(clingo_answers_list,expected_answers_list))
+	elif OPERATION == "contains":
+		c = contains(clingo_answers_list,expected_answers_list)
+		print(c)
+	else:
+		print("OPERAÇÃO INVÁLIDA")
+
+	return
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print(
-            "Usage: python3 sat-answers-comparator.py {expected answers file} < {clingo output file}"
-        )
-        print("To run the tests: python3 sat-answers-comparator.py test")
-        quit()
-    elif sys.argv[1] == "tests":
-        sys.argv.pop()
-        unittest.main()
-    else:
-        main(sys.argv)
+	main()
+
