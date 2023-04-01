a8cd70c598d72dc3671994bf69bd8a3e5b540241
Lorenzo Bertin Salvador <lorenzobs@usp.br>
2023-04-01 09:58:10 -0300

    fix: finish renumbering the soft constraints
    Signed-off-by: Lorenzo Bertin Salvador <lorenzobs@usp.br>

--- src/class-scheduler.sh
+++ src/class-scheduler.sh
@@ -17,7 +17,7 @@ INPUT_PARSER=$(find "$BASE_DIR" -name "parser-input-semestral.py")
 # Clingo input
 BASIC_CONSTRAINTS="$CONSTRAINTS_DIR/basic_constraints.lp"
 HARD_CONSTRAINTS=$(find "$CONSTRAINTS_DIR" -name "hc*[0-9].lp")
-SOFT_CONSTRAINTS=$(find "$CONSTRAINTS_DIR" -name "sc*[0-9].lp")
+SOFT_CONSTRAINTS=$(find "$CONSTRAINTS_DIR" -name "sc*[0-9].lp" | sort)
 MINIMIZE_SC="$CONSTRAINTS_DIR/minimize_sc.lp"
 PYTHON_OPS="$CONSTRAINTS_DIR/python_utils.lp"
 SC_METRICS="$CONSTRAINTS_DIR/sc_metrics.lp"
